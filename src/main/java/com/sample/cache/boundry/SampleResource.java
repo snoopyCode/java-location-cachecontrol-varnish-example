00c5fa4a87084869ad07f2835e222d25d3d0ec87
Roland <roland.bader@googlemail.com>
2016-02-29 23:40:24 +0100

    only cachecontrol in code

--- src/main/java/com/sample/cache/boundry/SampleResource.java
+++ src/main/java/com/sample/cache/boundry/SampleResource.java
@@ -20,8 +20,6 @@ import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
 
 import com.sample.cache.business.CacheControlExample;
-import com.sample.cache.business.HazelcastExample;
-import com.sample.cache.business.JCacheExample;
 
 
 @Stateless
@@ -29,44 +27,7 @@ import com.sample.cache.business.JCacheExample;
 public class SampleResource {
 	
 	@Inject
-	JCacheExample slow;
-	
-	@Inject
-	HazelcastExample hazelcast;
-	
-	@Inject
 	CacheControlExample cacheControl;
-
-	@GET
-	@Path("/jcache")
-	@Produces( MediaType.TEXT_PLAIN )
-	public Response locations(@Context HttpHeaders httpHeaders, @Context HttpServletResponse response,
-	        @Context HttpServletRequest request) {
-             	     
-        System.out.println("Start slow request");
-        long before = System.currentTimeMillis() / 1000L;
-        slow.doSlowOperation("1", "2");
-        long timetorun = System.currentTimeMillis() / 1000L - before;
-        System.out.println("End slow request");        
-		
-        return Response.status(Response.Status.OK).entity("It took "+timetorun+" to do the slow operation").build();
-
-	}
-	
-	@GET
-	@Path("/hazelcast")
-	@Produces( MediaType.TEXT_PLAIN )
-	public Response hazelcast(@Context HttpHeaders httpHeaders, @Context HttpServletResponse response,
-	        @Context HttpServletRequest request) {
-             	     
-		hazelcast.init();
-		
-		String savedData = hazelcast.saveData();
-        System.out.println("Saved data: "+savedData);        
-		
-        return Response.status(Response.Status.OK).entity("Saved data: "+savedData).build();
-
-	}
 	
 	@GET
 	@Path("/cachecontrol/")
@@ -86,7 +47,7 @@ public class SampleResource {
         
         ResponseBuilder builder = Response.status(Response.Status.OK);
         builder.cacheControl(cc);
-        builder.entity("It took "+timetorun+" to do the slow operation with "+latitude+" and "+longitude);
+        builder.entity("It took "+timetorun+" seconds to do the slow operation with "+latitude+" and "+longitude+"at "+(System.currentTimeMillis() / 1000L));
 		
         return builder.build();
 
